39cafb74a682ff62ac951cb5ab67efa976e181a6
TJPhoenix64 <tyler.jordan@gcsaz.org>
2025-09-01 22:05:14 -0700

    created queen class

---
+++ Queen.java
@@ -0,0 +1,73 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.imageio.ImageIO;
+
+public class Queen extends Piece {
+
+    BufferedImage blackQueen;
+    BufferedImage whiteQueen;
+
+    public Queen(int col, int row, int id) {
+        super(col, row, id, 'q');
+
+        try {
+            blackQueen = ImageIO.read(new File("Photos/black-rook.png"));
+            whiteQueen = ImageIO.read(new File("Photos/white-rook.png"));
+        } catch (IOException e) {
+        }
+    }
+
+    public Queen(int col, int row, int id, boolean isMoving) {
+        super(col, row, id, 'q', true);
+
+        try {
+            blackQueen = ImageIO.read(new File("Photos/black-rook.png"));
+            whiteQueen = ImageIO.read(new File("Photos/white-rook.png"));
+        } catch (IOException e) {
+        }
+    }
+
+    @Override
+    public ArrayList<MoveOption> getMoves() {
+        ArrayList<MoveOption> answer = new ArrayList<>();
+
+        Piece newPiece = getPiece(col, row);
+
+        int pieceId = newPiece.getId();
+        int pieceCol = newPiece.col;
+        int pieceRow = newPiece.row;
+
+        // diagonals
+        addMovesInDirection(answer, pieceCol, pieceRow, -1, -1, pieceId);
+        addMovesInDirection(answer, pieceCol, pieceRow, -1, 1, pieceId);
+        addMovesInDirection(answer, pieceCol, pieceRow, 1, 1, pieceId);
+        addMovesInDirection(answer, pieceCol, pieceRow, 1, -1, pieceId);
+
+        // straights
+        addMovesInDirection(answer, pieceCol, pieceRow, -1, 0, pieceId);
+        addMovesInDirection(answer, pieceCol, pieceRow, 1, 0, pieceId);
+        addMovesInDirection(answer, pieceCol, pieceRow, 0, 1, pieceId);
+        addMovesInDirection(answer, pieceCol, pieceRow, 0, -1, pieceId);
+
+        return answer;
+
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        if (blackQueen != null && id == 25) {
+            g.drawImage(blackQueen, super.x, super.y, width, height, null);
+            isWhite = false;
+        } else if (whiteQueen != null && id == 26) {
+            g.drawImage(whiteQueen, super.x, super.y, width, height, null);
+            isWhite = true;
+        } else {
+            g.setColor(Color.BLACK);
+            g.fillRect(super.x, super.y, width, height);
+        }
+    }
+}
